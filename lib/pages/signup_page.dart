b65cd75011e394c14127c41095e2d9cb157559d7
Haroon Imran <haroonimran231@gmail.com>
2023-08-01 16:22:59 +0500

    sfsdd

---
+++ lib/pages/signup_page.dart
@@ -0,0 +1,107 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:login/pages/login_page.dart';
+import 'package:login/widgets/round_button.dart';
+
+class SignUPPage extends StatefulWidget {
+  const SignUPPage({super.key});
+
+  @override
+  State<SignUPPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUPPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // automaticallyImplyLeading: false,
+        title: Center(child: Text("SignUP")),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
+                    decoration: InputDecoration(
+                        hintText: "Email",
+                        prefixIcon: Icon(Icons.email_outlined)),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please Enter Email";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                      keyboardType: TextInputType.text,
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: "Password",
+                          prefixIcon: Icon(Icons.lock_open)),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please Enter Password";
+                        }
+                        return null;
+                      }),
+                ],
+              ),
+            ),
+            SizedBox(height: 50),
+            RoundButton(
+              title: "Sign Up",
+              onTap: () {
+                if (_formKey.currentState!.validate()) {
+                  _auth.createUserWithEmailAndPassword(
+                      email: emailController.text.toString(),
+                      password: passwordController.text.toString());
+                }
+              },
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Already hve an account?"),
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+                    child: Text("Login"))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
